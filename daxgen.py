9a62d4ab30207b826adad255ed2ef673a2603f79
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-03-08 08:31:59 -0800

    enabling execution of the workflow outside ExoGeni

--- daxgen.py
+++ daxgen.py
@@ -13,13 +13,6 @@ parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
 parser.add_option('-d', '--dax', action='store', dest='daxfile', default='1000genome.dax', help='DAX filename')
 parser.add_option('-D', '--dataset', action='store', dest='dataset', default='20130502', help='Dataset folder')
 parser.add_option('-f', '--datafile', action='store', dest='datafile', default='data.csv', help='Data file with list of input data')
-parser.add_option('-y', '--priority', action='store', dest='priority', default='70', help='Workflow Priority')
-parser.add_option('-s', '--site', action='store', dest='sitename', default='S1', help='Site name')
-parser.add_option('-u', '--user', action='store', dest='user', default='panorama', help='RabbitMQ username')
-parser.add_option('-p', '--passwd', action='store', dest='passwd', default='panorama', help='RabbitMQ password')
-parser.add_option('-H', '--host', action='store', dest='host', default='128.9.136.208', help='RabbitMQ hostname or IP')
-parser.add_option('-P', '--port', action='store', dest='port', default='5671', help='RabbitMQ port')
-parser.add_option('-n', '--no-ssl', action='store_false', dest='ssl', default=True, help='RabbitMQ SSL connection')
 
 (options, args) = parser.parse_args()
 
@@ -30,30 +23,30 @@ workflow = ADAG("1000genome-%s" % options.dataset)
 
 # Executables
 e_individuals = Executable('individuals', arch='x86_64', installed=False)
-e_individuals.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/individuals', 'local'))
+e_individuals.addPFN(PFN('file://' + base_dir + '/bin/individuals', 'local'))
 workflow.addExecutable(e_individuals)
 
 e_individuals_merge = Executable('individuals_merge', arch='x86_64', installed=False)
-e_individuals_merge.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/individuals_merge', 'local'))
+e_individuals_merge.addPFN(PFN('file://' + base_dir + '/bin/individuals_merge', 'local'))
 workflow.addExecutable(e_individuals_merge)
 
 e_sifting = Executable('sifting', arch='x86_64', installed=False)
-e_sifting.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/sifting', 'local'))
+e_sifting.addPFN(PFN('file://' + base_dir + '/bin/sifting', 'local'))
 workflow.addExecutable(e_sifting)
 
 e_mutation = Executable('mutation_overlap', arch='x86_64', installed=False)
-e_mutation.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/mutation_overlap.py', 'local'))
+e_mutation.addPFN(PFN('file://' + base_dir + '/bin/mutation_overlap.py', 'local'))
 workflow.addExecutable(e_mutation)
 
 e_freq = Executable('frequency', arch='x86_64', installed=False)
-e_freq.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/frequency.py', 'local'))
+e_freq.addPFN(PFN('file://' + base_dir + '/bin/frequency.py', 'local'))
 workflow.addExecutable(e_freq)
 
 # Population Files
 populations = []
 for base_file in os.listdir('data/populations'):
   f_pop = File(base_file)
-  f_pop.addPFN(PFN('scp://adamant@master' + os.path.abspath('data/populations') + '/' + base_file, 'local'))
+  f_pop.addPFN(PFN('file://' + os.path.abspath('data/populations') + '/' + base_file, 'local'))
   workflow.addFile(f_pop)
   populations.append(f_pop)
 
@@ -75,7 +68,7 @@ for row in datacsv:
 
   # Individuals Jobs
   f_individuals = File(base_file)
-  f_individuals.addPFN(PFN('http://172.16.1.200/adamant/genome/data/' + options.dataset + '/' + base_file, 'compute'))
+  f_individuals.addPFN(PFN('file://' + base_dir + '/data/' + options.dataset + '/' + base_file, 'local'))
   workflow.addFile(f_individuals)
 
   c_num = base_file[base_file.find('chr')+3:]
@@ -96,10 +89,6 @@ for row in datacsv:
     j_individuals.uses(f_chrn, link=Link.OUTPUT, transfer=False)
     j_individuals.uses(f_columns, link=Link.OUTPUT, transfer=False)
     j_individuals.addArguments(f_individuals, c_num, str(counter), str(stop), str(threshold))
-
-    # Pre-script (RabbitMQ)
-    j_individuals.addProfile(Profile(Namespace.DAGMAN, 'PRE', os.getcwd() + '/bin/rabbitmq'))
-    j_individuals.addProfile(Profile(Namespace.DAGMAN, 'PRE.ARGUMENTS', '%s %s %s %s %s %s %s' % (options.user, options.passwd, options.priority, options.sitename, options.host, options.port, options.ssl)))
 
     individuals_jobs.append(j_individuals)
     workflow.addJob(j_individuals)
@@ -130,7 +119,7 @@ for row in datacsv:
   j_sifting = Job(name='sifting')
   
   f_sifting = File(row[2])
-  f_sifting.addPFN(PFN('http://172.16.1.200/adamant/genome/data/' + options.dataset + '/sifting/' + row[2], 'compute'))
+  f_sifting.addPFN(PFN('file://' + base_dir + '/data/' + options.dataset + '/sifting/' + row[2], 'local'))
   workflow.addFile(f_sifting)
 
   f_sifted = File('sifted.SIFT.chr%s.txt' % c_num)
