659a45fde60407ae703ce507d8d69c7657ac40bc
Tu Mai Anh Do <tudo@usc.edu>
2021-09-17 10:06:00 -0700

    Undo harcoding DECAF_DIR

--- env.sh
+++ env.sh
@@ -10,8 +10,9 @@ module load cmake
 module load boost/1.69.0
 
 # Decaf
-DECAF_LIBDIR=$(dirname "$(readlink -f "${BASH_SOURCE[-1]}")")
-DECAF_LIBDIR="${DECAF_LIBDIR}/lib"
+#DECAF_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[-1]}")")
+DECAF_DIR="/global/cfs/cdirs/m2187/pegasus-decaf/1000genome-workflow"
+DECAF_LIBDIR="${DECAF_DIR}/lib"
 export LD_LIBRARY_PATH="${DECAF_LIBDIR}:${LD_LIBRARY_PATH}"
 export PYTHONPATH="${DECAF_LIBDIR}:${PYTHONPATH}"
 
