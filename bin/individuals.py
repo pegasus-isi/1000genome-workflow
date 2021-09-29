c788d390d7c3b5c743034e89743b541687823816
Tu Mai Anh Do <tudo@usc.edu>
2021-09-28 20:59:35 -0700

    Resolve unbalance between individuals

--- bin/individuals.py
+++ bin/individuals.py
@@ -22,8 +22,8 @@ def processing(inputfile, columfile, c, counter, stop, total):
     tic = time.perf_counter()
 
     counter = int(counter)
-    ending = int(min(stop, total))
-    print("== Ending: {}".format(ending))
+    ending = min(int(stop), int(total))
+    
     ### step 0
     unzipped = 'ALL.chr{}.individuals.vcf'.format(c)
     # unzipped = os.path.splitext(inputfile)[0]  # Remove .gz
