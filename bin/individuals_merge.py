3373092e209da4e6a0b9e8e12e9977759a807d3b
George Papadimitriou <georgpap@isi.edu>
2022-02-02 13:45:01 -0800

    distinct merge working dirs

--- bin/individuals_merge.py
+++ bin/individuals_merge.py
@@ -9,31 +9,31 @@ import tempfile
 
 
 def compress(archive, input_dir):
-    with tarfile.open(archive, "w:gz") as file:
-        file.add(input_dir, arcname=os.path.basename(input_dir))
+    with tarfile.open(archive, "w:gz") as f:
+        f.add(input_dir, arcname=os.path.basename(input_dir))
 
 def extract_all(archive, output_dir):
-    with tarfile.open(archive, "r:*") as file:
-        file.extractall(os.path.basename(output_dir))
-        flist = file.getnames()
+    with tarfile.open(archive, "r:*") as f:
+        f.extractall(os.path.basename(output_dir))
+        flist = f.getnames()
         if flist[0] == '':
             flist = flist[1:]
         return flist
 
-def readfile(file):
-    with open(file, 'r') as f:
+def readfile(filename):
+    with open(filename, 'r') as f:
         content = f.readlines()
     return content
 
-def writefile(file, content):
-    with open(file, 'w') as f:
+def writefile(filename, content):
+    with open(filename, 'w') as f:
         f.writelines(content)
 
 def merging(c, tar_files):
     print('= Merging chromosome {}...'.format(c))
     tic = time.perf_counter()
 
-    merged_dir = 'merged/'
+    merged_dir = "merged_chr{}".format(c)
     os.makedirs(merged_dir, exist_ok=True)
 
     data = {}
@@ -41,17 +41,17 @@ def merging(c, tar_files):
     for tar in tar_files:
         tic_iter = time.perf_counter()
         with tempfile.TemporaryDirectory(dir=os.curdir) as temp_dir:
-            for file in extract_all(tar, temp_dir):
-                content = readfile(temp_dir+'/'+file)
-                if file in data:
-                    data[file] += content
+            for filename in extract_all(tar, temp_dir):
+                content = readfile(os.path.join(temp_dir, filename))
+                if filename in data:
+                    data[filename] += content
                 else:
-                    data[file] = content
+                    data[filename] = content
 
         print("Merged {} in {:0.2f} sec".format(tar, time.perf_counter()-tic_iter))
     
-    for file,content in data.items():
-        writefile(merged_dir+'/'+file, content)
+    for filename,content in data.items():
+        writefile(os.path.join(merged_dir, filename), content)
     
     outputfile = "chr{}n.tar.gz".format(c)
     print("== Done. Zipping {} files into {}.".format(len(data), outputfile))
